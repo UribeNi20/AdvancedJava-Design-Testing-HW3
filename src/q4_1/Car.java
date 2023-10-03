775d76c4b74be7cd9572725afa21466f399b6b3e
Nicolas Uribe <nicolasu2002@icloud.com>
2023-10-03 07:24:36 -0400

    Updated to fit critera of 4.1a

--- src/q4_1/Car.java
+++ src/q4_1/Car.java
@@ -1,37 +1,71 @@
 package q4_1;
 
-public class Car {
+import java.util.Date;
 
-    //private fields
-    private String _model;
-    private String _make;
-    private int _year;
+/**
+ * Represents a Car with attributes make, model, and built date.
+ * <p>
+ * Class Invariant:
+ * - The built date should never be in the future.
+ * </p>
+ */
+public final class Car {
 
-    //constructor
-    public Car(String make, String model, int year){
-    _model = model;
-    _make = make;
-    _year = year;
-    }
-    //getters
-    public String getModel(){
-        return _model;
-    }
-    public String getMake(){
-        return _make;
-    }
-    public int getYear(){
-        return _year;
+    private final String make;
+    private final String model;
+    private final Date builtDate;
+
+    /**
+     * Constructs a new Car instance.
+     * Preconditions: builtDate should not be in the future.
+     * Postconditions: A new Car instance is created with the specified attributes.
+     */
+    public Car(String make, String model, Date builtDate) {
+        this.make = make;
+        this.model = model;
+        this.builtDate = new Date(builtDate.getTime()); // Defensive copy to ensure immutability
     }
 
-    //setters
-    public void setModel(String model) {
-        _model = model;
+    /**
+     * Returns the make of the car.
+     *
+     * @return the make
+     * Preconditions: None
+     * Postconditions: The make of the car is returned.
+     */
+    public String getMake() {
+        return make;
     }
-    public void setMake(String make) {
-        _make = make;
+
+    /**
+     * Returns the model of the car.
+     *
+     * @return the model
+     * Preconditions: None
+     * Postconditions: The model of the car is returned.
+     */
+    public String getModel() {
+        return model;
     }
-    public void setYear(int year) {
-        _year = year;
+
+    /**
+     * Returns the built date of the car.
+     *
+     * @return the built date
+     * Preconditions: None
+     * Postconditions: The built date of the car is returned.
+     */
+    public Date getBuiltDate() {
+        return new Date(builtDate.getTime()); // Defensive copy to ensure immutability
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", builtDate=" + builtDate +
+                '}';
+    }
+
 }
